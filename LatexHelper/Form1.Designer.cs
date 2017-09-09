3e7a3a4b48a372915711c3d120dba59d0158203a
张 子苏 <ZhangZisu@outlook.com>
2017-09-09 16:39:00 +0800

    Build#2

--- LatexHelper/Form1.Designer.cs
+++ LatexHelper/Form1.Designer.cs
@@ -28,9 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.Preview = new System.Windows.Forms.GroupBox();
             this.webpreview = new System.Windows.Forms.WebBrowser();
+            this.MemoryScanner = new System.Windows.Forms.GroupBox();
+            this.latex = new System.Windows.Forms.TextBox();
+            this.source = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.settings = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.Preview.SuspendLayout();
+            this.MemoryScanner.SuspendLayout();
+            this.settings.SuspendLayout();
             this.SuspendLayout();
             // 
             // Preview
@@ -39,9 +51,11 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Preview.Controls.Add(this.webpreview);
-            this.Preview.Location = new System.Drawing.Point(12, 12);
+            this.Preview.Location = new System.Drawing.Point(14, 17);
+            this.Preview.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Preview.Name = "Preview";
-            this.Preview.Size = new System.Drawing.Size(260, 100);
+            this.Preview.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Preview.Size = new System.Drawing.Size(491, 124);
             this.Preview.TabIndex = 0;
             this.Preview.TabStop = false;
             this.Preview.Text = "Preview";
@@ -49,21 +63,126 @@
             // webpreview
             // 
             this.webpreview.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webpreview.Location = new System.Drawing.Point(3, 17);
-            this.webpreview.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webpreview.Location = new System.Drawing.Point(3, 20);
+            this.webpreview.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.webpreview.MinimumSize = new System.Drawing.Size(23, 28);
             this.webpreview.Name = "webpreview";
-            this.webpreview.Size = new System.Drawing.Size(254, 80);
+            this.webpreview.Size = new System.Drawing.Size(485, 100);
             this.webpreview.TabIndex = 0;
+            // 
+            // MemoryScanner
+            // 
+            this.MemoryScanner.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MemoryScanner.Controls.Add(this.button1);
+            this.MemoryScanner.Controls.Add(this.source);
+            this.MemoryScanner.Controls.Add(this.latex);
+            this.MemoryScanner.Location = new System.Drawing.Point(14, 149);
+            this.MemoryScanner.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MemoryScanner.Name = "MemoryScanner";
+            this.MemoryScanner.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MemoryScanner.Size = new System.Drawing.Size(491, 254);
+            this.MemoryScanner.TabIndex = 1;
+            this.MemoryScanner.TabStop = false;
+            this.MemoryScanner.Text = "Memory monitoring";
+            // 
+            // latex
+            // 
+            this.latex.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.latex.Location = new System.Drawing.Point(7, 28);
+            this.latex.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.latex.Multiline = true;
+            this.latex.Name = "latex";
+            this.latex.Size = new System.Drawing.Size(476, 83);
+            this.latex.TabIndex = 0;
+            // 
+            // source
+            // 
+            this.source.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.source.Location = new System.Drawing.Point(7, 122);
+            this.source.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.source.Multiline = true;
+            this.source.Name = "source";
+            this.source.Size = new System.Drawing.Size(476, 80);
+            this.source.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(7, 212);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(477, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Preview";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // settings
+            // 
+            this.settings.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.settings.Controls.Add(this.checkBox2);
+            this.settings.Controls.Add(this.checkBox1);
+            this.settings.Location = new System.Drawing.Point(14, 410);
+            this.settings.Name = "settings";
+            this.settings.Size = new System.Drawing.Size(491, 51);
+            this.settings.TabIndex = 2;
+            this.settings.TabStop = false;
+            this.settings.Text = "settings";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 22);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(109, 21);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Always on top";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(122, 22);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(103, 21);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Auto preview";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(519, 473);
+            this.Controls.Add(this.settings);
+            this.Controls.Add(this.MemoryScanner);
             this.Controls.Add(this.Preview);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MainForm";
             this.Text = "LatexHelper";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.Preview.ResumeLayout(false);
+            this.MemoryScanner.ResumeLayout(false);
+            this.MemoryScanner.PerformLayout();
+            this.settings.ResumeLayout(false);
+            this.settings.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -72,6 +191,15 @@
 
         private System.Windows.Forms.GroupBox Preview;
         private System.Windows.Forms.WebBrowser webpreview;
+        private System.Windows.Forms.GroupBox MemoryScanner;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox source;
+        private System.Windows.Forms.TextBox latex;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox settings;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- LatexHelper/Form1.cs
+++ LatexHelper/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace LatexHelper
 {
@@ -15,6 +17,72 @@ namespace LatexHelper
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            using (var fs = new FileStream("latex_preview.html", FileMode.Create))
+            {
+                using (var writer = new StreamWriter(fs))
+                {
+                    writer.Write("There is no Latex formula now...");
+                }
+            }
+            string path = Application.StartupPath;
+            webpreview.Url = new Uri("file://" + path + "/latex_preview.html");
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            File.Delete("latex_preview.html");
+            base.OnClosing(e);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                source.Text = Clipboard.GetText();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            TopMost = checkBox1.Checked;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            process_formula();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            process_formula();
+        }
+
+        private void process_formula()
+        {
+            string st = source.Text;
+            if (st.StartsWith(@"\[") && st.EndsWith(@"\]"))
+            {
+                string tex = st.Substring(2, st.Length - 4);
+                latex.Text = tex;
+                using (var fs = new FileStream("latex_preview.html", FileMode.Create))
+                {
+                    using (var writer = new StreamWriter(fs))
+                    {
+                        writer.Write(Resource1.HTMLHead);
+                        writer.Write(latex.Text);
+                        writer.Write(Resource1.HTMLEnd);
+                    }
+                }
+                webpreview.Refresh();
+            }
+            else
+            {
+                latex.Text = "The clipboard is empty or does not contain legal formulas.";
+            }
         }
     }
 }
